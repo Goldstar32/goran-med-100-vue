d6076784070eaca2dd89dff67d413c11042ea178
Goldstar32 <josua.karnsund@gmail.com>
2021-10-27 16:02:29 +0200

    Topplista och finare knappar

--- index.js
+++ index.js
@@ -10,38 +10,74 @@ var app = new Vue({
         menu: true,
         guessButton: true,
         loading: false,
+        highscoreArray: JSON.parse(localStorage.getItem('highScore') || "[]"),
+        difficulties: {
+            10: "Bebis",
+            50: "Lätt",
+            100: "Normal",
+            1000: "Svår",
+            1000000: "Omöjlig",
+        },
+        scene: "menu",
+        name: "",
     },
 
     methods: {
 
+        showMenu() {
+           this.scene = "menu"
+        },
+
+        showLoading() {
+            this.scene = "loading"
+        },
+        showGaming() {
+            this.scene = "gaming"
+            this.guessButton = "true"
+        },
+
         startGame(max) {
-            this.gaming = false
-            this.menu = false
-            this.loading = true
+            this.showLoading()
+            this.guesses = 0
             setTimeout(() => {
-                this.loading = false
-                this.guessButton = true
-                this.setResponseText("Vilket tal 1-" + max + " tänker Göran på?")
+                this.showGaming()
+                this.setResponseText(`Vilket tal 1-${max} tänker Göran på?`)
                 this.maxInput = max
-                this.gaming = true
                 this.goransNumber = this.getRndInteger(1, max)
             }, (3000))
-            
+        },
+        
+        createScore() {
+            var newHighscore = {
+                name: this.name,
+                difficulty: this.difficulties[this.maxInput],
+                score: this.guesses,
+            }
+            this.highscoreArray.push(newHighscore)
+            localStorage.setItem('highScore', JSON.stringify(this.highscoreArray))
+        },
+
+        answeredCorrect() {
+            this.guessButton = false
+            if (this.guesses != 1) {
+                this.setResponseText(`Rätt! Det tog bara ${this.guesses} gissningar!`)
+            } else {
+                this.setResponseText("Rätt! PÅ FÖRSTA FÖRSÖKET!")
+            }
+        },
+
+        showScoreboard() {
+            this.createScore()
+            this.scene = "scoreboard"
         },
 
         controll(guess) {
             if (this.goransNumber === guess) {
-                if (this.guesses != 1) {
-                    this.setResponseText("Rätt! Det tog bara " + this.guesses + " gissningar!")
-                } else {
-                    this.setResponseText("Rätt! PÅ FÖRSTA FÖRSÖKET!")
-                }
-                this.guessButton = false
-                this.menu = true
+                this.answeredCorrect()
             } else if (this.goransNumber < guess) {
-                this.setResponseText("Görans tal är lite mindre...")
+                this.setResponseText(`Görans tal är mindre än ${guess}`)
             } else if (this.goransNumber > guess) {
-                this.setResponseText("Görans tal är lite större...")
+                this.setResponseText(`Görans tal är större än ${guess}`)
             }
         },
 
