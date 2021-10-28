b9588cc83e3e2314066a6c45f5a47c98159aa9c4
Goldstar32 <josua.karnsund@gmail.com>
2021-10-28 14:08:14 +0200

    Finare meny och olika topplistor

--- index.js
+++ index.js
@@ -12,36 +12,55 @@ var app = new Vue({
         loading: false,
         highscoreArray: JSON.parse(localStorage.getItem('highScore') || "[]"),
         difficulties: {
-            10: "Bebis",
-            50: "Lätt",
-            100: "Normal",
-            1000: "Svår",
-            1000000: "Omöjlig",
+            10: "baby",
+            50: "easy",
+            100: "normal",
+            1000: "hard",
+            1000000: "impossible",
         },
         scene: "menu",
         name: "",
+        currentScoreboard: "",
     },
 
     computed: {
         reversedHighscore: function () {
-            return this.highscoreArray.sort((a, b) => {
-                return b.score - a.score
+            return this.highscoreArray
+            .filter((highscore) => {
+                return highscore.difficulty === this.currentScoreboard
+            })
+            .sort((a, b) => {
+                return a.score - b.score  
             }).slice(0,10)
         }
     },
 
     methods: {
 
+        setCurrentScoreboard(difficulty) {
+            this.currentScoreboard = difficulty
+        },
+
         showMenu() {
             this.scene = "menu"
         },
 
-        showLoading() {
-            this.scene = "loading"
+        showGameMenu() {
+            this.scene = "gameMenu"
         },
+
+        showScoreboard() {
+            this.createScore()
+            this.scene = "scoreboard"
+        },
+
         showGaming() {
             this.scene = "gaming"
             this.guessButton = "true"
+        },
+
+        showLoading() {
+            this.scene = "loading"
         },
 
         startGame(max) {
@@ -52,15 +71,15 @@ var app = new Vue({
                 this.setResponseText(`Vilket tal 1-${max} tänker Göran på?`)
                 this.maxInput = max
                 this.goransNumber = this.getRndInteger(1, max)
-            }, (3000))
+            }, (2000))
         },
 
         createScore() {
-            var calculatedScore = this.maxInput - this.guesses * this.maxInput / 10
+            //var calculatedScore = this.maxInput - this.guesses * this.maxInput / 10
             var newHighscore = {
                 name: this.name,
                 difficulty: this.difficulties[this.maxInput],
-                score: calculatedScore,
+                score: this.guesses,
             }
             this.highscoreArray.push(newHighscore)
             localStorage.setItem('highScore', JSON.stringify(this.highscoreArray))
@@ -73,11 +92,6 @@ var app = new Vue({
             } else {
                 this.setResponseText("Rätt! PÅ FÖRSTA FÖRSÖKET!")
             }
-        },
-
-        showScoreboard() {
-            this.createScore()
-            this.scene = "scoreboard"
         },
 
         controll(guess) {
